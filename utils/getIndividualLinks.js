a0610b631de18de14b2082f5b160909bd1441fee
Florescu_Calin <calinflorescu3@gmail.com>
2020-02-15 10:20:09 +0200

    get the number of pages from the website

--- utils/getIndividualLinks.js
+++ utils/getIndividualLinks.js
@@ -1,40 +1,51 @@
-const request = require('request-promise');
-const cheerio = require('cheerio');
-const {BASE_URL: baseUrl, NUMBER_OF_PAGES: pages} = require('../config');
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const { BASE_URL: baseUrl } = require("../config");
 
 const announces = [];
 
-const getLinks = async (url) => {
-    return request({
-        method: 'GET',
-        url: url
-    }).then(body => {
-        let $ = cheerio.load(body);
+const getNumberOfPages = async () => {
+  return request({
+    method: "GET",
+    url: baseUrl
+  }).then(body => {
+    let $ = cheerio.load(body);
 
-        $('.border-box h2 a').each((index, element) => {
-            const individualLink = $(element).attr('href');
-            announces.push(individualLink);
-        });
+    const numberOfPages = $(".butonpaginare:nth-child(7)").attr("data-pagina");
 
-        return announces;
+    return numberOfPages;
+  });
+};
+
+const getLinks = async url => {
+  return request({
+    method: "GET",
+    url: url
+  }).then(body => {
+    let $ = cheerio.load(body);
+
+    $(".border-box h2 a").each((index, element) => {
+      const individualLink = $(element).attr("href");
+      announces.push(individualLink);
     });
+
+    return announces;
+  });
 };
 
 const getAllLinks = async () => {
-    for(let i = 1; i < pages + 1; i++) {
-        if(i === 1) {
-            await getLinks(baseUrl);
-        } else {
-            const specificUrl = `${baseUrl}/?pagina=${i}`;
-            await getLinks(specificUrl);
-        }
-    }
+  const pages = await getNumberOfPages();
 
-    return announces;
+  for (let i = 1; i < pages + 1; i++) {
+    if (i === 1) {
+      await getLinks(baseUrl);
+    } else {
+      const specificUrl = `${baseUrl}/?pagina=${i}`;
+      await getLinks(specificUrl);
+    }
+  }
+
+  return announces;
 };
 
-getAllLinks().then(resp => console.log(resp));
-
-
-
-
+module.exports = getAllLinks;
